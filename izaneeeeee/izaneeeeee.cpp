222519c3de8f1a60810270cd53ffc72e3792f790
Amyas GHEROUS <amyas.gherous.dz@viacesi.fr>
2020-11-02 22:21:06 +0100

    Ajoutez des fichiers projet.

---
+++ izaneeeeee/izaneeeeee.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+int main()
+{
+	int Xa;
+
+	Xa = 5;
+
+	std::cin >> Xa;
+	std::cout << "Xa est de :" << (Xa);
+
+	/*std::cout << "Entrez la valeur du y du premier point : ";
+	std::cin >> Ya;
+	std::cout << "Entrez la valeur du x du deuxieme point : ";
+	std::cin >> Xb;
+	std::cout << "Entrez la valeur du y du deuxieme point : ";
+	std::cin >> Yb;
+	distancex = (Xb - Xa) ^ 2;
+	distancey = (Yb - Ya) ^ 2;
+	std::cout << "la distance de ces deux points sont de : " << sqrt(distancex + distancey);
+
+	Xa = 0;
+	std:: cin >> Xa;*/
+	return 0;
+}
